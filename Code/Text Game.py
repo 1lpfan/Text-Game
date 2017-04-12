96480dfdcf14baf8710c31d2b24516e17ba0a66d
1lpfan <mikkelson_andrew@ashwaubenonk12.org>
2017-04-12 16:18:08 -0500

    Added Time module

--- Code/Text Game.py
+++ Code/Text Game.py
@@ -3,10 +3,14 @@
 ###############################
 
 import random
+import time
 
 print('------------------------------------------')
 print('******Bunker: A Text Based Adventure******')
 print('------------------------------------------')
+
+# Wait 5 seconds
+time.sleep(5)
 
 #User enters their name and it is stored as player
 player = raw_input('What is your name?  ')
