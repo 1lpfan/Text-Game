213af6200ff713c935eacb48bb473ad9d26a1e43
1lpfan <mikkelson_andrew@ashwaubenonk12.org>
2017-04-12 16:33:06 -0500

    Added Background Information

--- Code/Text Game.py
+++ Code/Text Game.py
@@ -9,8 +9,17 @@ print('------------------------------------------')
 print('******Bunker: A Text Based Adventure******')
 print('------------------------------------------')
 
-# Wait 5 seconds
-time.sleep(5)
+# Wait 3 seconds
+time.sleep(3)
 
 #User enters their name and it is stored as player
 player = raw_input('What is your name?  ')
+time.sleep(1)
+
+# "Setting the scene"
+print('The date is April 2nd, 2132')
+time.sleep(1)
+print('You entered the bunker exactly five years ago when the bombs began to fall.')
+print('The nuclear war decimated Earth, leaving only pockets of survivors.')
+time.sleep(5)
+print('Today is the day you will leave the safety of your bunker because your supplies are dangerously low.')
