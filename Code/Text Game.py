6ec87200bf792864e1a1bcc248ac69cc3d67e32b
1lpfan <mikkelson_andrew@ashwaubenonk12.org>
2017-04-12 16:11:07 -0500

    Added player variable
    game askes player for their name and stores it as the variable player.

--- Code/Text Game.py
+++ Code/Text Game.py
@@ -7,3 +7,6 @@ import random
 print('------------------------------------------')
 print('******Bunker: A Text Based Adventure******')
 print('------------------------------------------')
+
+#User enters their name and it is stored as player
+player = raw_input('What is your name?  ')
