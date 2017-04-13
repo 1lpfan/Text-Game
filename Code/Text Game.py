81fb839e5d83de12da49148d2b19e82bacfb8e45
1lpfan <mikkelson_andrew@asd.lan>
2017-04-13 12:11:40 -0500

    Added Enemy Attack

--- Code/Text Game.py
+++ Code/Text Game.py
@@ -36,7 +36,7 @@ def intro():
     
 def attack(weapon_damage, enemy_hp):
     '''
-    Defines the parameters of basic combat.
+    Defines the parameters of attacking.
     weapon_damage is the max hp damage the weapon can deal.
     enemy_hp is the hp value of the enemy.
     '''
@@ -47,6 +47,20 @@ def attack(weapon_damage, enemy_hp):
         print('Enemy defeated!')
     else:
         print('Enemy Hit Points: '+ str(enemy_hp)) 
+                
+def enemy_attack(enemy_damage, player_hp):
+    '''
+    Defines the parameters of an enemy attacking the player.
+    enemy damage is the max hp damage the enemy can deal.
+    player_hp is the player's hp value.
+    '''
+    player_damage = random.randint(1, enemy_damage)
+    player_hp = player_hp - player_damage
+    time.sleep(.5)
+    if player_hp <= 0:
+        print('You have died!')
+    else:
+        print('Your Hit Points: ' + str(player_hp))
         
 
 
