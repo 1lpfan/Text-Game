46de865d0f705f4b5ae8507347cde82b0712b8c2
1lpfan <mikkelson_andrew@ashwaubenonk12.org>
2017-04-15 09:45:08 -0500

    Made player global

--- Code/Text Game.py
+++ Code/Text Game.py
@@ -19,6 +19,7 @@ def player_name():
     '''
     Asks the player for their name and stores it as variable player 
     ''' 
+    global player
     player = raw_input('What is your name? >')
     print('Welcome, '+player)
 
