ed5e244b6c95dd53baf8b0f0b52cf25fe58af5fe
1lpfan <dmikkelson17@gmail.com>
2017-04-18 11:39:04 -0500

    Moved functions into story
    Functions dealing with the game's story are now part of the module
    story.

--- Code/gamepack/story.py
+++ Code/gamepack/story.py
@@ -2,3 +2,31 @@
 #Story
 ######
 
+import time
+
+def display_title():
+    '''
+    Displays Game title
+    '''
+    print('------------------------------------------')
+    print('******Bunker: A Text Based Adventure******')
+    print('------------------------------------------')
+
+def player_name():
+    '''
+    Asks the player for their name and stores it as variable player 
+    ''' 
+    player = raw_input('What is your name? >')
+    print('Welcome, '+player)
+
+
+def intro():
+    '''
+    Itroduces the player to the game with some of the background story
+    '''
+    print('The date is April 2nd, 2132')
+    time.sleep(1)
+    print('You entered the bunker exactly five years ago when the bombs began to fall.')
+    print('The nuclear war decimated Earth, leaving only pockets of survivors.')
+    time.sleep(5)
+    print('Today is the day you will leave the safety of your bunker because your supplies are dangerously low.')
