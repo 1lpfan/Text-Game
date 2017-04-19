d0f506eb5375368bebaa9b93410b50ce259e29e8
1lpfan <dmikkelson17@gmail.com>
2017-04-19 11:44:37 -0500

    Added Spiders and Crabs

--- Code/gamepack/enemies.py
+++ Code/gamepack/enemies.py
@@ -10,3 +10,12 @@ class Enemy:
  
     def is_alive(self):
         return self.hp > 0
+        
+class Spider(Enemy):
+    def __init__(self):
+        super().__init__(name='Irradiated Spider', hp=10, damage=2)
+ 
+ 
+class Crab(Enemy):
+    def __init__(self):
+        super().__init__(name='Irradiated Crab', hp=30, damage=15)
