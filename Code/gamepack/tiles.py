2ece6e6aa1175aace00339d038c7720da30466cd
1lpfan <dmikkelson17@gmail.com>
2017-04-19 12:12:11 -0500

    Added player

--- Code/gamepack/tiles.py
+++ Code/gamepack/tiles.py
@@ -60,12 +60,12 @@ class EmptyCavePath(MapTile):
  
 class GiantSpiderRoom(EnemyRoom):
     def __init__(self, x, y):
-        super().__init__(x, y, enemies.GiantSpider())
+        super().__init__(x, y, enemies.Spider())
  
     def intro_text(self):
         if self.enemy.is_alive():
             return """
-            A giant spider jumps down from its web in front of you!
+            A giant irradiated spider jumps down from its web in front of you!
             """
         else:
             return """
