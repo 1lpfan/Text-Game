f5f1d855b026d347f5927b932b9f1e86d2270ef1
1lpfan <dmikkelson17@gmail.com>
2017-04-21 12:34:51 -0500

    fixed tiles

--- Code/gamepack/world.py
+++ Code/gamepack/world.py
@@ -2,6 +2,7 @@
 #World
 ######
 
+
 _world = {}
 starting_position = (0, 0)
  
@@ -13,7 +14,7 @@ def load_tiles():
     for y in range(len(rows)):
         cols = rows[y].split('\t')
         for x in range(x_max):
-            tile_name = cols[x].replace('\n', '') # Windows users may need to replace '\r\n'
+            tile_name = cols[x].replace('\r\n', '') # Windows users may need to replace '\r\n'
             if tile_name == 'StartingRoom':
                 global starting_position
                 starting_position = (x, y)
